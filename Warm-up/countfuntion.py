88358876f887af2ba43ed6fdd54dcb64c3e23ef8
Gurucharan Vemuru (gc) <72401652+Gurucharan-V@users.noreply.github.com>
2025-07-01 08:55:04 -0400

    Update countfuntion.py

--- Warm-up/countfuntion.py
+++ Warm-up/countfuntion.py
@@ -11,3 +11,17 @@ def countfuntion(n):
     return counter
 
 print(countfuntion(-))
+
+
+def countfuntion(n):
+    if n == 0:
+        return 1
+    n=abs(n)
+    counter = 0 
+    # for x in range(len(str(n))):
+    while(n>0):
+        n=n//10
+        counter +=1
+    return counter
+
+print(countfuntion(123123123))
