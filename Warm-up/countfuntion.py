decf1e664b1160b6d496cfaa8f077ec61e80dbf6
Gurucharan Vemuru (gc) <72401652+Gurucharan-V@users.noreply.github.com>
2025-07-01 08:55:23 -0400

    Update countfuntion.py

--- Warm-up/countfuntion.py
+++ Warm-up/countfuntion.py
@@ -1,8 +1,6 @@
 def countfuntion(n):
     if n == 0:
         return 1
-    elif n == -:
-        return "minus doesnt print here sheeeeh"
     n=abs(n)
     counter = 0 
     for x in range(len(str(n))):
@@ -10,7 +8,7 @@ def countfuntion(n):
         counter +=1
     return counter
 
-print(countfuntion(-))
+print(countfuntion(123))
 
 
 def countfuntion(n):
