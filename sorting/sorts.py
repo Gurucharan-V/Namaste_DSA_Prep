6ccc67166c694035939839d39bb99bdc89036450
Gurucharan Vemuru (gc) <72401652+Gurucharan-V@users.noreply.github.com>
2025-08-11 17:48:16 -0400

    Update sorts.py

--- sorting/sorts.py
+++ sorting/sorts.py
@@ -1,1 +1,42 @@
+ar = [3,2,1,4,0]
 
+
+def bubsrt(arr):
+    for i in range(len(arr)-1):
+        for j in range(len(arr)-1-i):
+            if arr[j+1] < arr[j]:
+                arr[j+1],arr[j] =arr[j],arr[j+1]
+                
+    return arr
+        
+print(bubsrt(ar))
+
+
+def selsrt(arr):
+    for i in range(len(arr)):
+        min = i
+        for j in range(i+1, len(arr)):
+            if arr[min] > arr[j]:
+                min = j
+            
+            arr[min],arr[i] = arr[i],arr[min]
+            
+    return arr
+    
+print(selsrt(ar))
+
+
+def isrtsrt(arr):
+    for i in range(1,len(arr)):
+        cur = i
+        prev = i-1
+        while(arr[prev] > arr[cur]):
+            arr[prev+1] = arr[prev]
+            prev -=1 
+            
+        arr[prev+1] = arr[cur]
+        
+        return arr
+        
+print(isrtsrt(ar))
+            
