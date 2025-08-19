05cc76d0b85bf5c121c6af285ee21686cdab8608
Gurucharan Vemuru (gc) <72401652+Gurucharan-V@users.noreply.github.com>
2025-08-19 00:00:37 -0400

    Update design_ll.py

--- MyLinkedList/design_ll.py
+++ MyLinkedList/design_ll.py
@@ -1,57 +1,68 @@
-class node:
+class Node:
     def __init__(self,val):
         self.val = val
         self.ref = None
+        
 class MyLinkedList:
     def __init__(self):
         self.head = None
         self.size = 0
-
-    def addAtHead(self, val: int) -> None:
-        newnode = node(val)
-        newnode.ref = self.head
-        self.head = newnode
-        self.size+=1
-
-    def addAtTail(self, val: int) -> None:
-        newnode = node(val)
-        if self.head == None:
-            self.head = newnode
-            self.size +=1
-        else:
-            temp = self.head
-            while temp.ref:
-                temp = temp.ref
-            temp.ref = newnode
-            self.size +=1
         
-    def addAtIndex(self,ind,val):
-        temp = self.head
-        newnode = node(val)
-        counter = 0
-        if ind == 0:
-            self.head = newnode
-            self.size +=1
-        else:
-            while counter < ind:
-                temp = temp.ref
-                counter += 1
-                
-            temp.ref = newnode
-            self.size +=1
-            
+    def addhead(self,val):
+        temp = Node(val)
+        temp.ref = self.head
+        self.head = temp
+        self.size+=1
+        
+    def addtail(self,val):
+        new = Node(val)
+        if self.size == 0:
+            self.head = new
 
+        temp = self.head
+        while temp.ref:
+            temp = temp.ref
+        temp.ref = new
+        
+        
+    def addindex(self,ind,val):
+        new = Node(val)
+        temp = self.head
+        counter = 0
+        
+        while counter < ind-1:
+            temp = temp.ref
+            counter+=1
+        new.ref = temp.ref
+        temp.ref = new
+        
+    def getindex(self, ind):
+        temp = self.head
+        counter = 0
+        while counter < ind:
+            temp = temp.ref
+            counter+=1
+        return temp.val
+        
+        
+        
     def printns(self):
         temp = self.head
-        
         while temp:
             print(temp.val)
             temp = temp.ref
-            
-            
+        
+        
 a = MyLinkedList()
-a.addAtHead(3)
-a.addAtTail(5)
-a.addAtIndex(1,4)
+
+a.addhead(11)
+a.addhead(10)
+a.addtail(13)
+
+a.addindex(2,12)
+
+
 a.printns()
-print(a.size)
+
+print(a.getindex(0))
+
